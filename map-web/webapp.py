6cccde62b86f090387a597e2343bae56a4007898
WilliPummer <willi.pummer@gmx.net>
2022-10-14 16:30:05 +0200

    table added to map-web

--- map-web/webapp.py
+++ map-web/webapp.py
@@ -1,10 +1,14 @@
+import json
 from datetime import datetime
 import logging
 import os
 import sys
 import uuid
+
+import requests
+
 import kafkaConsumer as kc
-from flask import Flask, Response, redirect, url_for, stream_with_context
+from flask import Flask, Response, redirect, url_for, stream_with_context, render_template
 
 app = Flask(__name__)
 
@@ -17,6 +21,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
+dist_url = os.environ['DIST_URL']
 hosts = os.environ['KAFKA_HOSTS']
 topic = os.environ['KAFKA_TOPIC']
 
@@ -26,6 +31,35 @@ logging.info(f'Connecting to {topic} on hosts {hosts}')
 @app.route('/')
 def hello_world():
     return redirect(url_for('static', filename='index.html'))
+
+
+@app.route('/table')
+def table():
+    response = requests.get(dist_url)
+
+    try:
+        response = requests.get(dist_url, timeout=5)
+        response.raise_for_status()
+        json = response.json()
+
+        for element in json:
+            element['dis'] = '{:.2f} km'.format(element['dis'] / 1000)
+            try:
+                element['date'] = datetime.fromtimestamp(element['date']/1000.0).strftime("%d/%m/%Y, %H:%M:%S")
+            except ValueError:
+                logging.info(element)
+
+        return render_template('table.html', title='Distance Table',
+                               entries=json)
+
+    except requests.exceptions.HTTPError as errh:
+        print(errh)
+    except requests.exceptions.ConnectionError as errc:
+        print(errc)
+    except requests.exceptions.Timeout as errt:
+        print(errt)
+    except requests.exceptions.RequestException as err:
+        print(err)
 
 
 @app.route('/stream')
@@ -38,7 +72,7 @@ def generate_large_csv():
                 if isinstance(msg, str):
                     yield "data: %s\n\n" % msg
                 else:
-                    date = datetime.fromtimestamp(msg["date"]/1000).strftime("%H:%M:%S")
+                    date = datetime.fromtimestamp(msg["date"] / 1000).strftime("%H:%M:%S")
                     data = f'{{"id": "{msg["id"]}","date": "{date}", "lat": "{msg["lat"]}", "lng": "{msg["lon"]}"}}'
                     yield "data: %s\n\n" % data
         except GeneratorExit:
